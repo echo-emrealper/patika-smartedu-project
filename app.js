8b90c316237b1a6bb3463ab641f2af282dba14aa
echo-emrealper <emrealper@mail.com>
2023-03-01 00:34:49 +0300

    Lesson 2 Topics

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 //MODULES
 const express = require('express');
+const ejs = require('ejs');
 
 //VARIABLES
 const app = express();
 const port = 3000;
 
-//ROUTERS
+// MIDDLEWARES
+app.use(express.static('public'));
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//ROUTES
 app.get('/', (req, res) => {
-  res.status(200).send('index sayfası');
+  res.status(200).render('index', {
+    page_name: 'index',
+  });
 });
 
-// LISTENERS
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: 'about',
+  });
+});
+
+// LISTENER
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
