23b323b1de20c77c5b910bc966ef81127155fc77
echo-emrealper <emrealper@mail.com>
2023-03-07 19:09:47 +0300

    Lessons 8 Topics

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 //VARIABLES
 const app = express();
@@ -28,6 +29,7 @@ app.set('view engine', 'ejs');
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 // LISTENER
 app.listen(port, () => {
