eb0a11790075ed572a7b3846124d4c36c7b2748d
echo-emrealper <emrealper@mail.com>
2023-03-14 16:41:12 +0300

    Lesson 20 Topics

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -21,4 +23,52 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+  const outputMessage = `
+  <h1>Message Detailes</h1>
+  <ul>
+    <li>Name:${req.body.name}</li> 
+    <li>Email:${req.body.email}</li> 
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}<p>
+  `;
+
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'berenice51@ethereal.email', // generated ethereal user
+      pass: 'YfwqkpvcsGKaFVGk2j', // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"SmartEDU Contact Team" <berenice51@ethereal.email>', // sender address
+    to: 'berenice51@ethereal.email', // list of receivers
+    subject: 'SmartEDU Contact Team', // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log('Message sent: %s', info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.status(200).redirect('/contact');
 };
