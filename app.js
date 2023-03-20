7b68d4336f48a802d5938f47c28953005316a9d2
echo-emrealper <emrealper@mail.com>
2023-03-20 20:50:45 +0300

    Port changed.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const userRoute = require('./routes/userRoute');
 
 //VARIABLES
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 global.userIN = null;
 
 // CONNECT
